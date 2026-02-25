e85db988327896d5badbee85925ff2a751b5c617
Vladimir <vovandrop@gmail.com>
2026-02-25 14:56:07 +0300

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/react-result/",
   plugins: [
     react(),
     VitePWA({
@@ -11,20 +12,52 @@ export default defineConfig({
       manifest: {
         name: "Notes",
         short_name: "Notes",
-        start_url: "/",
+        start_url: "/react-result/",
         display: "standalone",
         background_color: "#5d5b5b",
         theme_color: "#575151",
         orientation: "landscape",
         icons: [
-          { src: "/pwa-72x72.png", sizes: "72x72", type: "image/png" },
-          { src: "/pwa-96x96.png", sizes: "96x96", type: "image/png" },
-          { src: "/pwa-128x128.png", sizes: "128x128", type: "image/png" },
-          { src: "/pwa-144x144.png", sizes: "144x144", type: "image/png" },
-          { src: "/pwa-152x152.png", sizes: "152x152", type: "image/png" },
-          { src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-384x384.png", sizes: "384x384", type: "image/png" },
-          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "/react-result/pwa-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/react-result/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
       },
     }),
