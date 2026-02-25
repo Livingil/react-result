6d8fa24f846d39924691267808fb7df6768d6e54
Vladimir <vovandrop@gmail.com>
2026-02-25 15:14:28 +0300

    fix routing error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <ModalsProvider>
       <AuthProvider>
         <NotesProvider>
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <App />
           </BrowserRouter>
         </NotesProvider>
